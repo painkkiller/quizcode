71d083508f23f4b28053b69b1bccd884bd0f386c
Diana <IAmDianaWebDev@proton.me>
2023-10-28 20:06:24 +0300

    colors changed and some other stuff

--- src/content/overview/Navbar/index.js
+++ src/content/overview/Navbar/index.js
@@ -3,45 +3,66 @@ import { AppBar, Toolbar, Button, Typography } from '@mui/material';
 import Logo from 'src/components/LogoSign';
 
 const Nav = () => {
-  return (
-    <AppBar
-      position="static"
-      variant="elevation"
-      sx={{ background: '#ffff', padding: '15px' }}
-    >
-      <Toolbar>
-        <Logo />
-        <Typography
-          sx={{
-            color: '#4255ff',
-            fontSize: '25px',
-            fontWeight: '700',
-            marginRight: '15px'
-          }}
-        >
-          QuizCode
-        </Typography>
-        <Button sx={{ color: '#282F3F', fontSize: '16px'  }} variant="text">
-          Главная
-        </Button>
-        <Button sx={{ color: '#282F3F', fontSize: '16px'  }} variant="text">Тесты</Button>
-        <Button sx={{ marginLeft: 'auto', color: '#586381', fontSize: '16px' }}>
-          Войти
-        </Button>
-        <Button
-          variant="contained"
-          sx={{
-            marginLeft: '10px',
-            background: '#fddc63',
-            color: '#282F3F',
-            fontSize: '16px'
-          }}
-        >
-          Регистрация
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
+   return (
+      <AppBar
+         position="static"
+         variant="elevation"
+         sx={{ background: '#FFFFFF', padding: '15px' }}
+      >
+         <Toolbar>
+            <Logo />
+            <Typography
+               sx={{
+                  color: '#223355',
+                  fontSize: '25px',
+                  marginRight: '15px',
+                  fontWeight: 'bold',
+                  letterSpacing: 1
+               }}
+            >
+               QuizCode
+            </Typography>
+            <Button sx={{
+               color: '#223355', fontSize: '16px', textTransform: 'uppercase', fontWeight: 'light', letterSpacing: 1, '&:hover': {
+                  backgroundColor: '#fff'
+               }
+            }} variant="text">
+               Главная
+            </Button>
+            <Button sx={{
+               color: '#223355', fontSize: '16px', textTransform: 'uppercase', fontWeight: 'light', letterSpacing: 1, '&:hover': {
+                  backgroundColor: '#fff'
+               }
+            }} variant="text">Тесты</Button>
+            <Button sx={{
+               marginLeft: 'auto', color: '#223355', fontSize: '16px', textTransform: 'uppercase', fontWeight: 'light', letterSpacing: 1, '&:hover': {
+                  backgroundColor: '#223355',
+                  color: '#fff',
+               }
+            }}>
+               Войти
+            </Button>
+            <Button
+               variant="contained"
+               sx={{
+                  marginLeft: '10px',
+                  background: '#5568ff;',
+                  color: '#fff',
+                  fontSize: '16px',
+                  textTransform: 'uppercase',
+                  fontWeight: 'light',
+                  letterSpacing: 1,
+                  '&:hover': {
+                     backgroundColor: '#58c924',
+                     color: '#fff',
+                  }
+               }}
+            >
+               Регистрация
+            </Button>
+         </Toolbar>
+      </AppBar>
+   );
 };
 
 export default Nav;
