a010aa41c487593d7de947d5091a1293a49b2477
Dmitry Malyugin <Dmitry.Malyugin@center2m.ru>
2023-11-05 15:15:05 +0300

    Stepper

--- src/content/applications/Course/index.jsx
+++ src/content/applications/Course/index.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Container, Typography, styled } from '@mui/material';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import { Container, Typography, Box, styled } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { loadCourse } from './courseSlice';
@@ -9,6 +12,7 @@ import { loadCourse } from './courseSlice';
 const CourseWrapper = styled(Container)(
   ({ theme }) => `
     margin: ${theme.spacing(4)};
+    width: auto;
 `
 );
 
@@ -37,12 +41,40 @@ function Course() {
     }
     return "Wrong title";
   }
+
+  const getBody = () => {
+    if (!topicId) {
+      return course?.desc;
+    } else if (topicId && course?.topics && !subId) {
+      return course?.topics[topicId]?.body;
+    } else if (subId && course?.topics) {
+      return course?.topics[topicId].subs[Number(subId)]?.body;
+    }
+    return "Wrong body";
+  }
+
+  const getStepper = () => {
+    if (course?.topics && course?.topics[topicId] && course?.topics[topicId].subs.length > 1) {
+      return (
+        <Box sx={{ width: '100%' }}>
+          <Stepper activeStep={subId}>
+            {
+              course?.topics[topicId].subs.map((sub, i) => <Step key={i} completed={subId > i}><StepLabel> </StepLabel></Step>)
+            }
+          </Stepper>
+        </Box>
+      );
+    }
+    return null;
+  }
   
 
   return (
     <CourseWrapper>
-      Курс { courseId }, { topicId } , { subId }
-      <Typography variant='h1'>{getTitle()}</Typography>
+      { courseId }, { topicId } , { subId }
+      { getStepper() }
+      <Typography variant='h1' gutterBottom>{getTitle()}</Typography><br/>
+      <Typography variant="body1" gutterBottom>{getBody()}</Typography><br/>
     </CourseWrapper>
   );
 }
